bfcd4db1573c4e8d69d56d8754b7c93c4f6ac8fe
rees_s <simon.rees@psi.ch>
2018-08-12 11:23:08 +0200

    #6759: Add support for CORS. Now targets the gfa-autodeploy server. Checks whether link status widget present before trying to manipulate it. Still needs plenty of cleaning up !

--- src/main/resources/static/wica.js
+++ src/main/resources/static/wica.js
@@ -10,13 +10,13 @@ function set_server_link_status_indicator( ok ) {
 }
 
 
-function find_epics_aware_widgets() {
+function find_epics_aware_elements() {
     return document.querySelectorAll('[data-epics-channel]');
 }
 
 function create_event_stream( ev_callback) {
 
-    var epics_widgets = find_epics_aware_widgets();
+    var epics_widgets = find_epics_aware_elements();
     console.log( "Epics widgets found: !", epics_widgets.length );
 
     var channels = new Array();
@@ -25,7 +25,8 @@ function create_event_stream( ev_callback) {
         channels.push( channelName );
     } );
 
-    var SUBSCRIBE_CHANNEL_URL= "../ca/streams";
+    //var SUBSCRIBE_CHANNEL_URL= "http://localhost:8080/ca/streams/";
+    var SUBSCRIBE_CHANNEL_URL= "https://gfa-autodeploy.psi.ch:8443/ca/streams/";
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (xhttp.readyState === XMLHttpRequest.DONE && xhttp.status === 200) {
@@ -35,6 +36,8 @@ function create_event_stream( ev_callback) {
         }
     };
     var jsonChannelString = JSON.stringify( channels );
+    xhttp.onerror = function() { console.log( "XHTTP error when subscribing to channel" ); };
+    xhttp.withCredentials = true;
     xhttp.open( "POST", SUBSCRIBE_CHANNEL_URL, true);
     xhttp.setRequestHeader( "Content-Type", "application/json" );
     xhttp.send( jsonChannelString );
@@ -42,20 +45,22 @@ function create_event_stream( ev_callback) {
 
 function connect_event_stream( id, ev_callback )
 {
-    var EVENT_STREAM_URL_BASE= "ca/streams/";
+    //var EVENT_STREAM_URL_BASE= "http://localhost:8080/ca/streams/";
+    var EVENT_STREAM_URL_BASE= "https://gfa-autodeploy.psi.ch:8443/ca/streams/";
     var eventStreamUrl = EVENT_STREAM_URL_BASE + id;
 
-    var eventSource = new EventSource( eventStreamUrl );
+    var eventSource = new EventSource( eventStreamUrl, { withCredentials: true } );
     eventSource.addEventListener( 'message', function(e) {
 
-        // Perform recommended security check
-        var expectedOrigin = location.origin;
-        if ( e.origin !== expectedOrigin )
-        {
-            console.warn( "Event source: 'stream' unexpected event origin." );
-            return false;
-        }
-
+        // The code block below could be used to perform a security check
+        // but for the moment at PSI it is disabled as the page may be located
+        // on a different server from wica.
+        // var expectedOrigin = location.origin;
+        // if ( e.origin !== expectedOrigin )
+        // {
+        //     console.warn( "Event source: 'stream' unexpected event origin." );
+        //     return false;
+        // }
         var eventData = JSON.parse( e.data );
 
         ev_callback( eventData );
@@ -64,7 +69,9 @@ function connect_event_stream( id, ev_callback )
 
     eventSource.addEventListener( 'open', function() {
         console.log( "Event source: 'stream' - open event." );
-        set_server_link_status_indicator( true );
+        if ( linkStatusWidget != null ) {
+            set_server_link_status_indicator(true);
+        }
         process_wica_server_connect();
     }, false );
 
@@ -72,7 +79,9 @@ function connect_event_stream( id, ev_callback )
         console.log( "Event source: 'stream'  - error event. Closing event source." );
         eventSource.close();
         process_wica_server_disconnect();
-        set_server_link_status_indicator( false );
+        if ( linkStatusWidget != null ) {
+            set_server_link_status_indicator( false );
+        }
     }, false );
 }
 
@@ -94,7 +103,7 @@ function manage_event_stream() {
     }, ONE_SECOND_IN_TIMER_UNITS );
 
     function gotActivity( eventData ) {
-        console.log( "Event source: 'stream' has activity" );
+        //console.log( "Event source: 'stream' has activity" );
         countdownInSeconds = STREAM_TIMEOUT_INTERVAL;
         process_event_stream_update( eventData );
     }
@@ -102,32 +111,31 @@ function manage_event_stream() {
 
 function process_event_stream_update( eventData )
 {
-    find_epics_aware_widgets().forEach( function(widget) {
+    find_epics_aware_elements().forEach( function( element ) {
 
-        var channelName = widget.getAttribute( "data-epics-channel");
+        var channelName = element.getAttribute( "data-epics-channel");
         var channelValue= eventData[ channelName ];
         if (channelValue == null ) {
-            widget.textContent= "Channel Disconnected"
+            element.textContent= "Channel Disconnected"
         }
         else {
-            widget.textContent= channelValue;
+            element.textContent= channelValue;
         }
+        element.dispatchEvent( new Event('change') );
     } );
 }
 
 function process_wica_server_connect()
 {
-    find_epics_aware_widgets().forEach( function( widget ) {
-        var channelName = widget.getAttribute( "data-epics-channel");
-        widget.textContent= "Server Link Up"
+    find_epics_aware_elements().forEach( function( element  ) {
+        element.textContent= "Server Link Up"
     } );
 }
 
 function process_wica_server_disconnect()
 {
-    find_epics_aware_widgets().forEach( function( widget ) {
-        var channelName = widget.getAttribute( "data-epics-channel");
-        widget.textContent= "Server Link Down"
+    find_epics_aware_elements().forEach( function( element ) {
+        element.textContent= "Server Link Down"
     } );
 }
 
