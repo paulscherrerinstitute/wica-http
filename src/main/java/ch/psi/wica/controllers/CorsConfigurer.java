5a887c46eb81e5b4b5c7f99c28377feb4aac7d7a
rees_s <simon.rees@psi.ch>
2018-08-12 11:20:59 +0200

    #6759: Add support for CORS.

---
+++ src/main/java/ch/psi/wica/controllers/CorsConfigurer.java
@@ -0,0 +1,51 @@
+/*- Package Declaration ------------------------------------------------------*/
+
+package ch.psi.wica.controllers;
+
+/*- Imported packages --------------------------------------------------------*/
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+/*- Interface Declaration ----------------------------------------------------*/
+/*- Class Declaration --------------------------------------------------------*/
+
+@Configuration
+class CorsConfigurer implements WebMvcConfigurer
+{
+
+/*- Public attributes --------------------------------------------------------*/
+/*- Private attributes -------------------------------------------------------*/
+
+   private final Logger logger = LoggerFactory.getLogger(EventStreamController.class );
+
+/*- Main ---------------------------------------------------------------------*/
+/*- Constructor --------------------------------------------------------------*/
+/*- Class methods ------------------------------------------------------------*/
+/*- Public methods -----------------------------------------------------------*/
+
+   @Override
+   public void addCorsMappings( CorsRegistry registry )
+   {
+      logger.info( "Configuring CORS...");
+      registry.addMapping("/**")
+               // The definition below means that when Safari and Chrome load a web
+               // page from ANY webserver they will be able to make XHR and SSE
+               // requests on this server.
+              .allowedOrigins( "*" )
+               // Firefox is pickier: as '*' is not an allowed origin the
+               // origins must be enabled by hand.
+              .allowedOrigins( "http://localhost:8000", "https://intranet.psi.ch", "http://pc9013.psi.ch:8000", "file://" )
+              .allowCredentials( true );
+      logger.info( "CORS configuration completed.");
+   }
+
+/*- Private methods ----------------------------------------------------------*/
+/*- Nested Classes -----------------------------------------------------------*/
+
+}
+
