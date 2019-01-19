5a603dba882449e4610041fe4dda5a3be6c06c10
rees_s <simon.rees@psi.ch>
2019-01-19 16:28:14 +0100

    Merge remote-tracking branch 'origin/master'

--- src/main/resources/static/wica/document-stream-connector.js
+++ src/main/resources/static/wica/document-stream-connector.js
@@ -7,39 +7,11 @@ console.debug( "Executing script in document-stream-connector.js module...");
 import {WicaStreamManager} from './stream-manager.js'
 import * as DocumentUtilities from './document-utils.js'
 
+import {WicaChannelConnectionAttributes} from './definitions.js'
 
 //const WICA_HOST = "https://gfa-wica.psi.ch";
 const WICA_HOST = "https://gfa-wica-dev.psi.ch";
 
-
-/**
- * Attributes of a wica-aware HTML element that are used when communicating with the Wica backend server.
- *
- * @typedef WicaChannelConnectionAttributes
- * @property {string} CHANNEL_NAME - "data-wica-channel-name" - The attribute which defines the channel name.
- * @property {string} CHANNEL_PROPERTIES - "data-wica-channel-properties" - The attribute which defines the channel
- *     properties.
- * @property {string} CHANNEL_STREAM_STATE - "data-wica-stream-state" - The attribute which reflects the state of the
- *     connection to the backend server data stream.
- * @property {string} CHANNEL_CONNECTION_STATE - "data-wica-channel-connection-state" - The attribute which reflects
- *     the state of connection between the backend server and the channel's data source.
- * @property {string} CHANNEL_METADATA - "data-wica-channel-metadata" - The attribute which reflects the metadata
- *     obtained from the channel.
- * @property {string} CHANNEL_VALUE_ARRAY - "data-wica-channel-value-array" - The attribute which reflects the
- *     values most recently obtained from the channel.
- * @property {string} CHANNEL_VALUE_LATEST - "data-wica-channel-value-latest" - The attribute which reflects the last
- *     value obtained from the channel.
- * @property {string} CHANNEL_ALARM_STATE - "data-wica-channel-alarm-state" - The attribute which reflects whether
- *     the latest alarm status obtained from the channel.
- */
- const wicaChannelConnectionAttributes = { CHANNEL_NAME:             "data-wica-channel-name",
-                                                 CHANNEL_PROPERTIES:       "data-wica-channel-properties",
-                                                 CHANNEL_STREAM_STATE:     "data-wica-channel-stream-state",
-                                                 CHANNEL_CONNECTION_STATE: "data-wica-channel-connection-state",
-                                                 CHANNEL_METADATA:         "data-wica-channel-metadata",
-                                                 CHANNEL_VALUE_ARRAY:      "data-wica-channel-value-array",
-                                                 CHANNEL_VALUE_LATEST:     "data-wica-channel-value-latest",
-                                                 CHANNEL_ALARM_STATE:      "data-wica-channel-alarm-state" };
 /**
  * Provides real-time updates on the attributes of one or more of the current document's
  * wica-aware elements based on information streamed from a Wica backend data server.
@@ -52,8 +24,10 @@ export class DocumentStreamConnector
      * The returned object will remain in a dormant state until triggered by a call to the activate method.
 
      * @param {string} streamServerUrl - the URL of the backend server from whom information is to be obtained.
+     *
      * @param {StreamProperties} streamProperties - The properties of the stream that will be created to obtain the
      *     required information from the data sources.
+     *
      * @param {WicaChannelConnectionAttributes} wicaChannelConnectionAttributes - The names of the wica-aware
      *     element attributes that are used in the communication process.
      */
