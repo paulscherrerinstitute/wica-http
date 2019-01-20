29c40271e34aa65c43c33dbc87b1d4181188c46e
rees_s <simon.rees@psi.ch>
2019-01-20 18:20:17 +0100

    Merge remote-tracking branch 'origin/master'

--- src/main/resources/static/wica/shared-definitions.js
+++ src/main/resources/static/wica/shared-definitions.js
@@ -64,20 +64,20 @@ const WicaElementChannelRenderingProperties = {
     prec: 8
 };
 
-/**
- * Object defining the properties supported by a WicaStream. See <a><
- *
- * @global
- * @typedef WicaStreamProperties
- * @property {number} heartbeatInterval - The interval in milliseconds to be used between the
- *     sending of successive heartbeat messages.
- * @property {number} channelValueUpdateInterval The interval in milliseconds to be used between
- *     the sending of successive channel value updates.
- * @property {boolean} includeAlarmState - Whether alarm information should be included in channel
- *     value updates. Needed if the visual state of the element should change when in the alarm state.
- * @property {boolean} includeTimeStamp - Whether timestamp information should be included in channel
- *     value updates. Needed for time plots.
- */
+// /**
+//  * Object defining the properties supported by a WicaStream.
+//  *
+//  * @global
+//  * @typedef WicaStreamProperties
+//  * @property {number} heartbeatInterval - The interval in milliseconds to be used between the
+//  *     sending of successive heartbeat messages.
+//  * @property {number} channelValueUpdateInterval The interval in milliseconds to be used between
+//  *     the sending of successive channel value updates.
+//  * @property {boolean} includeAlarmState - Whether alarm information should be included in channel
+//  *     value updates. Needed if the visual state of the element should change when in the alarm state.
+//  * @property {boolean} includeTimeStamp - Whether timestamp information should be included in channel
+//  *     value updates. Needed for time plots.
+//  */
 
 /**
  * Object defining the properties supported by a WicaStream.
