698845917e7bbc45269ae4b40f5c7c8fb496c8e1
rees_s <simon.rees@psi.ch>
2019-02-03 12:42:31 +0100

    Changed event manager scan rate to 200ms.

--- src/main/resources/static/wica/wica.js
+++ src/main/resources/static/wica/wica.js
@@ -7,5 +7,5 @@ console.debug( "Executing script in wica.js module...");
 
 import * as DocumentServiceSupportLoader from './document-support-loader.js'
 
-DocumentServiceSupportLoader.load( 200, 1000 );
+DocumentServiceSupportLoader.load( 200, 200 );
 
