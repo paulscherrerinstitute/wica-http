4948c2003904c0fb981d910587b8efa9328996e7
rees_s <simon.rees@psi.ch>
2019-02-12 14:29:51 +0100

    Got latest from CDN

--- src/main/resources/static/wica/document-support-loader.js
+++ src/main/resources/static/wica/document-support-loader.js
@@ -15,7 +15,7 @@ import {DocumentStreamConnector} from "./document-stream-connector.js";
 import {DocumentTextRenderer} from "./document-text-renderer.js";
 import {DocumentEventManager} from "./document-event-manager.js";
 
-import * as JSON5 from "./json5-wrapper";
+import * as JSON5 from "./json5-wrapper.js";
 
 const WICA_HOST="https://gfa-wica-dev.psi.ch";
 
