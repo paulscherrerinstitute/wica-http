6c7688626478efb479a4f6be90f536761c39de25
rees_s <simon.rees@psi.ch>
2019-02-26 22:58:09 +0100

    Provided custom ES6 logger

--- src/main/resources/static/wica/client-api.js
+++ src/main/resources/static/wica/client-api.js
@@ -1,5 +1,6 @@
 /**
- * Provides the entrypoint API for leveraging all functionality associated with Wica.
+ * Provides the entrypoint API for leveraging all functionality associated with Wica
+ * from the Javascript client side.
  * @module
  */
 
@@ -16,20 +17,20 @@ export {
      * Class providing a facility for buffering the received information from one
      * or more wica-aware elements, subsequently making it available to third-parties
      * who may wish to poll for it.
-     * See {@link module:plot-buffer.PlotBuffer PlotBuffer}.
+     * See {@link module:plot-buffer~PlotBuffer PlotBuffer}.
      */
     PlotBuffer,
 
     /**
      * Class providing support for creating a new WicaStream on the Wica server, for
      * subscribing to it and for publishing locally the received information.
-     * See {@link module:stream-manager.StreamManager StreamManager}.
+     * See {@link module:stream-manager~StreamManager StreamManager}.
      */
     StreamManager,
 
     /**
      * Class providing all the functionality necessary to support a wica-aware html page.
-     * See {@link module:document-support-loader.DocumentSupportLoader DocumentSupportLoader}.
+     * See {@link module:document-support-loader~DocumentSupportLoader DocumentSupportLoader}.
      */
     DocumentSupportLoader
 }
