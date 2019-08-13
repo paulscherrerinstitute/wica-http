fee28845a4575d07ce45b1bf4de1b8458aa8269b
rees_s <simon.rees@psi.ch>
2019-08-13 12:29:42 +0200

    Added support for generating a key for accessing the data values in a map of polled or monitored channel values.

---
+++ src/main/java/ch/psi/wica/model/app/WicaDataBufferStorageKey.java
@@ -0,0 +1,77 @@
+/*- Package Declaration ------------------------------------------------------*/
+package ch.psi.wica.model.app;
+
+/*- Imported packages --------------------------------------------------------*/
+
+import ch.psi.wica.model.channel.WicaChannel;
+import net.jcip.annotations.Immutable;
+import org.apache.commons.lang3.Validate;
+
+import java.util.Objects;
+
+
+/*- Interface Declaration ----------------------------------------------------*/
+/*- Class Declaration --------------------------------------------------------*/
+
+@Immutable
+public class WicaDataBufferStorageKey
+{
+
+/*- Public attributes --------------------------------------------------------*/
+/*- Private attributes -------------------------------------------------------*/
+
+   private WicaChannel wicaChannel;
+   private int storageKey;
+
+/*- Main ---------------------------------------------------------------------*/
+/*- Constructor --------------------------------------------------------------*/
+
+   private WicaDataBufferStorageKey( WicaChannel wicaChannel, int storageKey )
+   {
+      this.wicaChannel = wicaChannel;
+      this.storageKey = storageKey;
+   }
+
+/*- Class methods ------------------------------------------------------------*/
+
+   public static WicaDataBufferStorageKey getPolledValueStorageKey( WicaChannel wicaChannel )
+   {
+      Validate.notNull( wicaChannel );
+      final int hashCode = (Objects.hash( wicaChannel.getName(), wicaChannel.getProperties().getOptionalPollingIntervalInMillis() ) );
+      return new WicaDataBufferStorageKey( wicaChannel, hashCode );
+   }
+
+   public static WicaDataBufferStorageKey getMonitoredValueStorageKey( WicaChannel wicaChannel )
+   {
+      Validate.notNull( wicaChannel );
+      final int hashCode = (Objects.hash( wicaChannel.getName().getControlSystemName() ) );
+      return new WicaDataBufferStorageKey( wicaChannel, hashCode );
+   }
+
+/*- Public methods -----------------------------------------------------------*/
+
+   public WicaChannel getWicaChannel()
+   {
+      return wicaChannel;
+   }
+
+   @Override
+   public boolean equals( Object o )
+   {
+      if ( this == o ) return true;
+      if ( !(o instanceof WicaDataBufferStorageKey) ) return false;
+      WicaDataBufferStorageKey that = (WicaDataBufferStorageKey) o;
+      return storageKey == that.storageKey;
+   }
+
+   @Override
+   public int hashCode()
+   {
+      return Objects.hash( storageKey );
+   }
+
+/*- Protected methods --------------------------------------------------------*/
+/*- Private methods ----------------------------------------------------------*/
+/*- Nested Classes -----------------------------------------------------------*/
+
+}
