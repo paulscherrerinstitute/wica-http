833a03d3ab366aafe5a4cb729d61e883b8266620
rees_s <simon.rees@psi.ch>
2019-08-20 17:51:50 +0200

    Switched tests to hamcrest style.

--- src/test/java/ch/psi/wica/services/channel/WicaChannelValueRateLimitingFilterTest.java
+++ src/test/java/ch/psi/wica/services/channel/WicaChannelValueRateLimitingFilterTest.java
@@ -9,8 +9,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 
 /*- Interface Declaration ----------------------------------------------------*/
@@ -49,12 +50,13 @@ class WicaChannelValueRateLimitingFilterTest
       final List<WicaChannelValue> inputList = List.of( strValue1, strValue2, strValue3, strValue4, strValue5 );
       final WicaChannelValueFilter mapper = new WicaChannelValueRateLimitingFilter(90 );
       final List<WicaChannelValue> outputList  = mapper.apply(inputList );
-      assertEquals( 5 , outputList.size() );
-      assertEquals( inputList.get( 0 ), outputList.get( 0 ) );
-      assertEquals( inputList.get( 1 ), outputList.get( 1 ) );
-      assertEquals( inputList.get( 2 ), outputList.get( 2 ) );
-      assertEquals( inputList.get( 3 ), outputList.get( 3 ) );
-      assertEquals( inputList.get( 4 ), outputList.get( 4 ) );
+
+      assertThat( outputList.size(), is( 5 ) );
+      assertThat( outputList.get( 0 ), is( inputList.get( 0 ) ) );
+      assertThat( outputList.get( 1 ), is( inputList.get( 1 ) ) );
+      assertThat( outputList.get( 2 ), is( inputList.get( 2 ) ) );
+      assertThat( outputList.get( 3 ), is( inputList.get( 3 ) ) );
+      assertThat( outputList.get( 4 ), is( inputList.get( 4 ) ) );
    }
 
    @Test
@@ -73,10 +75,11 @@ class WicaChannelValueRateLimitingFilterTest
       final List<WicaChannelValue> inputList = List.of( strValue1, strValue2, strValue3, strValue4, strValue5 );
       final WicaChannelValueFilter mapper = new WicaChannelValueRateLimitingFilter(110 );
       final List<WicaChannelValue> outputList  = mapper.apply(inputList );
-      assertEquals( 3 , outputList.size() );
-      assertEquals( inputList.get( 0 ), outputList.get( 0 ) );
-      assertEquals( inputList.get( 2 ), outputList.get( 1 ) );
-      assertEquals( inputList.get( 4 ), outputList.get( 2 ) );
+
+      assertThat( outputList.size(), is( 3) );
+      assertThat( outputList.get( 0 ), is( inputList.get( 0 ) ) );
+      assertThat( outputList.get( 1 ), is( inputList.get( 2 ) ) );
+      assertThat( outputList.get( 2 ), is( inputList.get( 4 ) ) );
    }
 
    @Test
@@ -86,24 +89,27 @@ class WicaChannelValueRateLimitingFilterTest
       final List<WicaChannelValue> inputList1 = List.of( strValue1 );
       final WicaChannelValueFilter mapper = new WicaChannelValueRateLimitingFilter(110 );
       final List<WicaChannelValue> outputList1  = mapper.apply(inputList1 );
-      assertEquals( 1 , outputList1.size() );
-      assertEquals( inputList1.get( 0 ), outputList1.get( 0 ) );
+
+      assertThat( outputList1.size(), is( 1 ) );
+      assertThat( outputList1.get( 0 ), is( inputList1.get( 0 ) ) );
 
       final WicaChannelValue strValue2 = WicaChannelValue.createChannelValueConnected( "def" );
       final List<WicaChannelValue> inputList2 = List.of( strValue2 );
       final List<WicaChannelValue> outputList2  = mapper.apply(inputList2 );
-      assertEquals( 0 , outputList2.size() );
+
+      assertThat( outputList2.size(), is( 0 ) );
 
       Thread.sleep( 120 );
       final WicaChannelValue strValue3 = WicaChannelValue.createChannelValueConnected( "ghi" );
       final List<WicaChannelValue> inputList3 = List.of( strValue3 );
-      final List<WicaChannelValue> outputList3  = mapper.apply(inputList3 );
-      assertEquals( 1 , outputList3.size() );
-      assertEquals( inputList3.get( 0 ), outputList3.get( 0 ) );
+      final List<WicaChannelValue> outputList3  = mapper.apply( inputList3 );
+
+      assertThat( outputList3.size(), is( 1 ) );
+      assertThat( outputList3.get( 0 ), is( inputList3.get( 0 ) ) );
    }
 
 
-   /*- Private methods ----------------------------------------------------------*/
+/*- Private methods ----------------------------------------------------------*/
 /*- Nested Classes -----------------------------------------------------------*/
 
 }
