8c456c3ef784b84029edbb16e91f174b01cce9a4
rees_s <simon.rees@psi.ch>
2019-09-04 14:44:16 +0200

    Further efforts to get travis build automation working.

--- startup.docker.sh
+++ startup.docker.sh
@@ -10,9 +10,7 @@ export CONFIG=/Users/simon/wica/config
 docker run --name wica-stream -t --rm \
   -p $TARGET_PORT:80 \
   -p 5062:5062/udp -p 5062:5062/tcp -p 5064:5064/udp -p 5064:5064/tcp -p 5065:5065/udp \
-  -v $WEBDIR:/root/web \
+  -v $WEBDIR:/public/web \
   --volumes-from wica-js-container \
   -e EPICS_CA_ADDR_LIST=192.168.1.116 \
   paulscherrerinstitute/wica-stream:$RELEASE
-
-
