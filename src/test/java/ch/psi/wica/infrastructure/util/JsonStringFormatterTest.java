946e1af0560c5406dd9cf41df8065ad9117ab34c
rees_s <simon.rees@psi.ch>
2020-03-13 01:24:07 +0100

    Issue #14 Improve tests.

---
+++ src/test/java/ch/psi/wica/infrastructure/util/JsonStringFormatterTest.java
@@ -0,0 +1,36 @@
+/*- Package Declaration ------------------------------------------------------*/
+package ch.psi.wica.infrastructure.util;
+
+/*- Imported packages --------------------------------------------------------*/
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+
+/*- Interface Declaration ----------------------------------------------------*/
+/*- Class Declaration --------------------------------------------------------*/
+
+class JsonStringFormatterTest
+{
+
+/*- Public attributes --------------------------------------------------------*/
+/*- Private attributes -------------------------------------------------------*/
+/*- Main ---------------------------------------------------------------------*/
+/*- Constructor --------------------------------------------------------------*/
+/*- Class methods ------------------------------------------------------------*/
+/*- Public methods -----------------------------------------------------------*/
+
+   @Test
+   void testPrettyPrint()
+   {
+      final String result = JsonStringFormatter.prettyFormat( "{\"value1\":\"NaN\",\"value2\":\"Infinity\"  }" );
+      assertThat( result, is( "{\n  \"value1\" : \"NaN\",\n  \"value2\" : \"Infinity\"\n}" ) );
+
+   }
+
+/*- Private methods ----------------------------------------------------------*/
+/*- Nested Classes -----------------------------------------------------------*/
+
+}
