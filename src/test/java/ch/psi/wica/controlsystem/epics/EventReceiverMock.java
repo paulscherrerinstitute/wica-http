06e3a5fb615994a06bf52c9ba397555253eafea8
rees_s <simon.rees@psi.ch>
2020-11-17 09:48:56 +0100

    Issue #51: Further polling improvements. Added support to detect WicaChannelPolledValueUpdateEvent.

--- src/test/java/ch/psi/wica/controlsystem/epics/EventReceiverMock.java
+++ src/test/java/ch/psi/wica/controlsystem/epics/EventReceiverMock.java
@@ -1,7 +1,12 @@
+/*- Package Declaration ------------------------------------------------------*/
+
 package ch.psi.wica.controlsystem.epics;
+
+/*- Imported packages --------------------------------------------------------*/
 
 import ch.psi.wica.controlsystem.event.WicaChannelMetadataUpdateEvent;
 import ch.psi.wica.controlsystem.event.WicaChannelMonitoredValueUpdateEvent;
+import ch.psi.wica.controlsystem.event.WicaChannelPolledValueUpdateEvent;
 import ch.psi.wica.model.channel.WicaChannelMetadata;
 import ch.psi.wica.model.channel.WicaChannelValue;
 import org.springframework.context.event.EventListener;
@@ -11,13 +16,27 @@ import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicReference;
 
+
+/*- Interface Declaration ----------------------------------------------------*/
+/*- Class Declaration --------------------------------------------------------*/
+
 @Component
 public class EventReceiverMock
 {
-   private AtomicReference<LocalDateTime> metadataPublished = new AtomicReference<>();
-   private AtomicReference<LocalDateTime> valuePublished = new AtomicReference<>();
-   private AtomicReference<WicaChannelMetadata> metadata = new AtomicReference<>();
-   private AtomicReference<WicaChannelValue> value = new AtomicReference<>();
+
+/*- Public attributes --------------------------------------------------------*/
+/*- Private attributes -------------------------------------------------------*/
+
+   private final AtomicReference<LocalDateTime> metadataPublished = new AtomicReference<>();
+   private final AtomicReference<LocalDateTime> valuePublished = new AtomicReference<>();
+   private final AtomicReference<WicaChannelMetadata> metadata = new AtomicReference<>();
+   private final AtomicReference<WicaChannelValue> value = new AtomicReference<>();
+
+
+/*- Main ---------------------------------------------------------------------*/
+/*- Constructor --------------------------------------------------------------*/
+/*- Class methods ------------------------------------------------------------*/
+/*- Public methods -----------------------------------------------------------*/
 
    public void arm()
    {
@@ -26,33 +45,50 @@ public class EventReceiverMock
    }
 
    @EventListener
-   public void handleWicaChannelMonitoredValueUpdateEvent ( WicaChannelMonitoredValueUpdateEvent event)
+   public void handleWicaChannelMonitoredValueUpdateEvent( WicaChannelMonitoredValueUpdateEvent event)
    {
       value.set( event.getWicaChannelValue() );
       valuePublished.set( LocalDateTime.now());
    }
 
    @EventListener
-   public void handleWicaChannelMetadataUpdateEvent ( WicaChannelMetadataUpdateEvent event )
+   public void handleWicaChannelMonitoredMetadataUpdateEvent( WicaChannelMetadataUpdateEvent event )
    {
       metadata.set( event.getWicaChannelMetadata() );
       metadataPublished.set( LocalDateTime.now() );
    }
 
+   @EventListener
+   public void handleWicaChannelPolledValueUpdateEvent( WicaChannelPolledValueUpdateEvent event )
+   {
+      value.set( event.getWicaChannelValue() );
+      valuePublished.set( LocalDateTime.now());
+   }
+
+
+/*- Package-level methods ----------------------------------------------------*/
+
    Optional<LocalDateTime> getMetadataPublishedTimestamp()
    {
       return Optional.ofNullable( metadataPublished.get() );
    }
+
    Optional<LocalDateTime> getValuePublishedTimestamp()
    {
       return Optional.ofNullable( valuePublished.get() );
    }
+
    Optional<WicaChannelMetadata> getMetadata()
    {
       return Optional.ofNullable( metadata.get() );
    }
+
    Optional<WicaChannelValue> getValue()
    {
       return Optional.ofNullable( value.get() );
    }
+
+/*- Private methods ----------------------------------------------------------*/
+/*- Nested Classes -----------------------------------------------------------*/
+
 }
