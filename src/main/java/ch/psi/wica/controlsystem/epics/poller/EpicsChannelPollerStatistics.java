340f6f9032f12b8554de21a2f5903773b255c1e0
rees_s <simon.rees@psi.ch>
2022-09-13 21:38:41 +0200

    Fix #95 Improve Javadoc.

--- src/main/java/ch/psi/wica/controlsystem/epics/poller/EpicsChannelPollerStatistics.java
+++ src/main/java/ch/psi/wica/controlsystem/epics/poller/EpicsChannelPollerStatistics.java
@@ -11,18 +11,20 @@ import org.apache.commons.lang3.Validate;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
 
 
 /*- Interface Declaration ----------------------------------------------------*/
 /*- Class Declaration --------------------------------------------------------*/
 
+/**
+ * Provides statistics related to the EPICS channel pollers.
+ */
 @ThreadSafe
 public class EpicsChannelPollerStatistics implements StatisticsCollectable
 {
 
-   /*- Public attributes --------------------------------------------------------*/
-   /*- Private attributes -------------------------------------------------------*/
+/*- Public attributes --------------------------------------------------------*/
+/*- Private attributes -------------------------------------------------------*/
 
    private final AtomicInteger startRequests = new AtomicInteger(0);
    private final AtomicInteger stopRequests = new AtomicInteger(0);
@@ -36,6 +38,12 @@ public class EpicsChannelPollerStatistics implements StatisticsCollectable
 /*- Main ---------------------------------------------------------------------*/
 /*- Constructor --------------------------------------------------------------*/
 
+
+   /**
+    * Creates a new EPICS channel poller statistics collector.
+    *
+    * @param requestMap initial list of poller requests.
+    */
    public EpicsChannelPollerStatistics( Map<EpicsChannelPollerRequest, EpicsChannelPollerPublisher.Poller> requestMap )
    {
       this.requestMap = Validate.notNull( requestMap );
@@ -69,49 +77,95 @@ public class EpicsChannelPollerStatistics implements StatisticsCollectable
       pollFailureCount.set( 0 );
    }
 
+   /**
+    * Returns the names of the channels being polled.
+    *
+    * @return the list.
+    */
+
    public List<String> getChannelNames()
    {
       return requestMap
-            .keySet()
-            .stream()
-            .map( req -> req.getPublicationChannel().getName().asString() )
-            .collect(Collectors.toUnmodifiableList() );
+              .keySet()
+              .stream()
+              .map(req -> req.getPublicationChannel().getName().asString()).toList();
    }
 
+   /**
+    * Returns the count of received EPICS channel poller start requests.
+    *
+    * @return the result.
+    */
    public String getStartRequests()
    {
       return String.valueOf( startRequests.get());
    }
 
+   /**
+    * Returns the count of received EPICS channel poller stop requests.
+    *
+    * @return the result.
+    */
    public String getStopRequests()
    {
       return String.valueOf( stopRequests.get());
    }
 
+   /**
+    * Returns the count of EPICS channel pollers that are currently active.
+    *
+    * @return the result.
+    */
    public String getActiveRequests()
    {
       return String.valueOf( requestMap.size() );
    }
 
+   /**
+    * Returns the count of connected EPICS channels.
+    *
+    * @return the result.
+    */
    public String getChannelConnectCount()
    {
       return String.valueOf( channelConnectCount.get() );
    }
+
+   /**
+    * Returns the count of disconnected EPICS channels.
+    *
+    * @return the result.
+    */
    public String getChannelDisconnectCount()
    {
       return String.valueOf( channelDisconnectCount.get() );
    }
 
+   /**
+    * Returns the count of EPICS polling cycles.
+    *
+    * @return the result.
+    */
    public String getPollCycleCount()
    {
       return String.valueOf( pollCycleCount );
    }
 
+   /**
+    * Returns the count of EPICS polling cycles that have been successful.
+    *
+    * @return the result.
+    */
    public String getPollSuccessCount()
    {
       return String.valueOf( pollSuccessCount );
    }
 
+   /**
+    * Returns the count of EPICS polling cycles that have been failed.
+    *
+    * @return the result.
+    */
    public String getPollFailureCount()
    {
       return String.valueOf( pollFailureCount );
@@ -119,28 +173,51 @@ public class EpicsChannelPollerStatistics implements StatisticsCollectable
 
 /*- Package-access methods ---------------------------------------------------*/
 
+   /**
+    * Increments the count of start requests.
+    */
    void incrementStartRequests()
    {
       startRequests.incrementAndGet();
    }
+
+   /**
+    * Increments the count of stop requests.
+    */
    void incrementStopRequests()
    {
       stopRequests.incrementAndGet();
    }
+
+   /**
+    * Increments the count of connected channels.
+    */
    void incrementChannelConnectCount()
    {
       channelConnectCount.incrementAndGet();
    }
+
+   /**
+    * Increments the count of disconnected channels.
+    */
    void incrementChannelDisconnectCount()
    {
       channelDisconnectCount.incrementAndGet();
    }
 
+   /**
+    * Increments the count of polling cycles.
+    */
    void incrementPollCycleCount()
    {
       pollCycleCount.incrementAndGet();
    }
 
+   /**
+    * Updates the polling result statistics.
+    *
+    * @param success token indicating whether the last poll attempt was successful.
+    */
    void updatePollingResult( boolean success )
    {
       if ( success )
