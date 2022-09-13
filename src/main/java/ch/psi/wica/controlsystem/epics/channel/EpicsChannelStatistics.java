7a9a9569e7490cf87d2c3440697b519275939b94
rees_s <simon.rees@psi.ch>
2022-09-13 21:38:04 +0200

    Fix #95 Improve Javadoc.

--- src/main/java/ch/psi/wica/controlsystem/epics/channel/EpicsChannelStatistics.java
+++ src/main/java/ch/psi/wica/controlsystem/epics/channel/EpicsChannelStatistics.java
@@ -13,12 +13,13 @@ import org.epics.ca.ConnectionState;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
-
 
 /*- Interface Declaration ----------------------------------------------------*/
 /*- Class Declaration --------------------------------------------------------*/
 
+/**
+ * Provides the statistics associated with an EPICS channel.
+ */
 @ThreadSafe
 public class EpicsChannelStatistics implements StatisticsCollectable
 {
@@ -26,7 +27,7 @@ public class EpicsChannelStatistics implements StatisticsCollectable
 /*- Public attributes --------------------------------------------------------*/
 /*- Private attributes -------------------------------------------------------*/
 
-   private final String instanceSpecifier;
+   private final String contextSpecifier;
    private final AtomicInteger createChannelRequests = new AtomicInteger(0);
    private final AtomicInteger removeChannelRequests = new AtomicInteger(0);
    private final Map<EpicsChannelName, Channel<Object>> channels;
@@ -35,9 +36,16 @@ public class EpicsChannelStatistics implements StatisticsCollectable
 /*- Main ---------------------------------------------------------------------*/
 /*- Constructor --------------------------------------------------------------*/
 
-   public EpicsChannelStatistics( String instanceSpecifier, Map<EpicsChannelName, Channel<Object>> channels )
+   /**
+    * Creates a new instance based on the supplied channel data.
+    *
+    * @param contextSpecifier a string specifying the context (for example polling or monitoring) associated
+    *    with these statistics.
+    * @param channels the channel map.
+    */
+   public EpicsChannelStatistics( String contextSpecifier, Map<EpicsChannelName, Channel<Object>> channels )
    {
-      this.instanceSpecifier = Validate.notEmpty( instanceSpecifier );
+      this.contextSpecifier = Validate.notEmpty( contextSpecifier );
       this.channels = Validate.notNull( channels );
    }
 
@@ -47,7 +55,7 @@ public class EpicsChannelStatistics implements StatisticsCollectable
    @Override
    public Statistics get()
    {
-      return new Statistics( "EPICS CHANNEL MANAGER SERVICE - " + instanceSpecifier.toUpperCase(),
+      return new Statistics( "EPICS CHANNEL MANAGER SERVICE - " + contextSpecifier.toUpperCase(),
                              List.of( new StatisticsItem("- Channels: Create Channel Requests", getCreateChannelRequests() ),
                                       new StatisticsItem("- Channels: Remove Channel Requests", getRemoveChannelRequests() ),
                                       new StatisticsItem("- Channels: Active Channel Requests", getActiveRequests() ),
@@ -64,68 +72,115 @@ public class EpicsChannelStatistics implements StatisticsCollectable
       removeChannelRequests.set( 0 );
    }
 
+   /**
+    * Returns the channel names.
+    *
+    * @return the channel names.
+    */
    public List<String> getChannels()
    {
       return channels
-            .keySet()
-            .stream()
-            .map( EpicsChannelName::toString )
-            .collect(Collectors.toUnmodifiableList() );
+              .keySet()
+              .stream()
+              .map(EpicsChannelName::toString).toList();
    }
 
    public List<String> getUnconnectedChannels()
    {
       return channels
-            .keySet()
-            .stream()
-            .filter( channel -> channels.get( channel ).getConnectionState() != ConnectionState.CONNECTED )
-            .map( EpicsChannelName::toString )
-            .collect( Collectors.toUnmodifiableList() );
+              .keySet()
+              .stream()
+              .filter(channel -> channels.get(channel).getConnectionState() != ConnectionState.CONNECTED)
+              .map(EpicsChannelName::toString).toList();
    }
-   
+
+   /**
+    * Returns a string representation of the number of create channel requests.
+    *
+    * @return the result.
+    */
    public String getCreateChannelRequests()
    {
       return String.valueOf( createChannelRequests.get());
    }
+
+   /**
+    * Returns a string representation of the number of remove channel requests.
+    *
+    * @return the result.
+    */
    public String getRemoveChannelRequests()
    {
       return String.valueOf( removeChannelRequests.get());
    }
+
+   /**
+    * Returns a string representation of the number of channel requests that are currently active.
+    *
+    * @return the result.
+    */
    public String getActiveRequests()
    {
       return String.valueOf( channels.size() );
    }
 
+   /**
+    * Returns a string representation of the number of connected channels.
+    *
+    * @return the result.
+    */
    public String getConnectedChannelCount()
    {
       final long monitoredChannelCount = channels.values().stream().filter( channel -> channel.getConnectionState() == ConnectionState.CONNECTED ).count();
       return String.valueOf( monitoredChannelCount );
    }
 
+   /**
+    * Returns a string representation of the number of channels that have never connected.
+    *
+    * @return the result.
+    */
    public String getNeverConnectedChannelCount()
    {
       final long neverConnectedChannelCount = channels.values().stream().filter( channel -> channel.getConnectionState() == ConnectionState.NEVER_CONNECTED ).count();
       return String.valueOf( neverConnectedChannelCount );
    }
 
+   /**
+    * Returns a string representation of the number of channels that are currently disconnected.
+    *
+    * @return the result.
+    */
    public String getDisconnectedChannelCount()
    {
       final long disconnectedChannelCount = channels.values().stream().filter( channel -> channel.getConnectionState() == ConnectionState.DISCONNECTED ).count();
      return String.valueOf( disconnectedChannelCount   );
    }
+
+   /**
+    * Returns a string representation of the number of channels that have been closed.
+    *
+    * @return the result.
+    */
    public String getClosedChannelCount()
    {
       final long closedChannelCount = channels.values().stream().filter( channel -> channel.getConnectionState() == ConnectionState.CLOSED ).count();
       return String.valueOf( closedChannelCount );
    }
-   
 
 /*- Package-access methods ---------------------------------------------------*/
 
+   /**
+    * Increments the count of create monitored channel requests.
+    */
    void incrementCreateMonitoredChannelRequests()
    {
       createChannelRequests.incrementAndGet();
    }
+
+   /**
+    * Increments the count of removed monitored channel requests.
+    */
    void incrementRemoveMonitoredChannelRequests()
    {
       removeChannelRequests.incrementAndGet();
